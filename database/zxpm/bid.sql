7f736731fb980ee425cb0333f2f71b5a29612685
Yue.Lee <liyueshining@gmail.com>
2015-03-09 14:19:21 +0800

    Update bid.sql

--- database/zxpm/bid.sql
+++ database/zxpm/bid.sql
@@ -1,1 +1,11 @@
-
+CREATE TABLE bid (
+  BidId int(11) NOT NULL AUTO_INCREMENT,
+  BuyerId int(11) NOT NULL,
+  GoodsId int(11) NOT NULL,
+  BidTime datetime NOT NULL,
+  BidPrice float NOT NULL,
+  BidStatus int(11) NOT NULL,
+  PRIMARY KEY (BidId),
+  FOREIGN KEY (BuyerId) REFERENCES users(UserId),
+  FOREIGN KEY (GoodsId) REFERENCES goods(GoodsId)
+);
